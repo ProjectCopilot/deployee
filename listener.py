5cfb0f41e9e4f810504e66d617bc6a9ea8e74196
Ankit Ranjan <me@ankit.io>
2016-07-10 00:56:29 -0700

    Removed debug parameter

--- listener.py
+++ listener.py
@@ -34,5 +34,5 @@ def webhook():
     return 'OK'
 
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=True)
+    app.run(host=app.config['HOST'], port=app.config['PORT'])
 
