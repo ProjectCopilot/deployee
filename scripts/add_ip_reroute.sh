8a1c407cdea4b6c5a3a3d66143aecd30e4c7a175
Ankit Ranjan <me@ankit.io>
2016-07-11 20:59:06 -0700

    Fix arg number

--- scripts/add_ip_reroute.sh
+++ scripts/add_ip_reroute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-if ! iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port ${2}; then
+if ! iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port ${1}; then
     echo "Are you root?"
 fi
 
