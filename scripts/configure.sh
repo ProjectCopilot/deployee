205ba2d73d5b361fc1d6bfbe1f95bf2c0465a933
Ankit Ranjan <me@ankit.io>
2016-07-11 20:45:04 -0700

    Added config script, resolves #4

---
+++ scripts/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eo pipefail
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+CONFIG=$BASEDIR/settings.cfg
+
+echo "What is your GitHub webhook secret?"
+read HUB_SECRET
+echo "What hostname do you want to run the listener on?"
+read HOST
+echo "What port do you want to run the listener on?"
+read PORT
+
+cat <<EOF > $CONFIG
+HUB_SECRET = '$HUB_SECRET'
+HOST = '$HOST'
+PORT = $PORT
+EOF
+
