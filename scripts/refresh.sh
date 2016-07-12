0a628efa83fb069d9e8cc7de9fd34a5edf2e458d
Ankit Ranjan <me@ankit.io>
2016-07-11 19:19:32 -0700

    Added quiet option to refresh.sh

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -3,10 +3,29 @@ set -eo pipefail
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 
+usage() {
+    echo "Usage: $0 [-q] <component>"
+}
+
 if [[ $# -eq 0 ]] ; then
-    echo "Which component do you want to deploy?"
-    exit 1
+    usage
+    exit 127
 fi
+
+# Check for flag
+OPTIND=1
+while getopts "qh" o ; do # set $o to the next passed option
+  case "$o" in 
+    q) QUIET=true 
+       ;;
+    h) print "$usage"
+       exit
+       ;;
+  esac
+done
+
+# Shift to the first legit argument
+shift $(($OPTIND - 1))
 
 cd $BASEDIR/components/"$1"
 git pull origin master
@@ -14,6 +33,10 @@ git pull origin master
 # Refresh background process"
 tmux kill-session -t "$1" &>/dev/null || true
 tmux new -s "$1" -d
-tmux send -t "$1" "bash deploy.sh 2>$(tty)" ENTER
+if [ $QUIET ]; then
+    tmux send -t "$1" "bash deploy.sh" ENTER
+else
+    tmux send -t "$1" "bash deploy.sh 2>$(tty)" ENTER
+fi
 sleep 1 # Wait for response
 
