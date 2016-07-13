5830af6d75f831d7d3b199188c82d8c3a207c6ef
Ankit Ranjan <me@ankit.io>
2016-07-13 00:04:22 -0700

    changed tmplist to pipe to close #8

--- scripts/cycle_components.sh
+++ scripts/cycle_components.sh
@@ -2,15 +2,11 @@
 set -eo pipefail
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
-TMPLIST=/tmp/deployee_component_list.csv
 
-# Yes, yes, I know. How about you figure out string manipulation in bash? 
-python $BASEDIR/scripts/list_components.py $TMPLIST
-
-while read p; do
+python $BASEDIR/scripts/list_components.py | while read -r p; do
     echo "Starting $p..."
     $BASEDIR/scripts/refresh.sh -gq $p
-done <$TMPLIST
+done
 
 echo "Started all components."
 
--- scripts/list_components.py
+++ scripts/list_components.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import os
-import sys
 
 basedir = os.path.dirname(os.path.abspath(__file__))
 with open(basedir + '/../data/projects.csv') as i:
-    # You're gonna love this one.
-    with open(sys.argv[1], 'w') as o:
-        # And here's the killer.
-        for link in i.read().splitlines():
-            o.write(link.rsplit('/', 1)[1][:-4] + '\n')
+    for link in i.read().splitlines():
+        print(link.rsplit('/', 1)[1][:-4])
 
